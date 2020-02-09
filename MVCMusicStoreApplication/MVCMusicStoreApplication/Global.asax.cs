c22a9d08e2d14b518f1140520faedeee0f5c70b8
Daniel Norris <Daniel.J.Norris@icloud.com>
2020-02-09 16:32:17 -0500

    MVCMusicStoreApplication Commit

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using MVCMusicStoreApplication.Models;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MVCMusicStoreApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new SampleData());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}

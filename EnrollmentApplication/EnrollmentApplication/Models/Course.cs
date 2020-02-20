fd3de87165d7bed7d76cd99a100cee679f72f050
Daniel Norris <Daniel.J.Norris@icloud.com>
2020-02-20 07:42:28 -0500

    Commit and push of Lab 6 to master

--- EnrollmentApplication/EnrollmentApplication/Models/Course.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -1,10 +1,21 @@
-﻿namespace EnrollmentApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
 {
     public class Course
     {
+        [Display(Name = "Course ID")]
         public virtual long CourseID { get; set; }
+        [Required]
+        [Display(Name = "Course Title")]
+        [MaxLength(length: 150)]
         public virtual string CourseTitle { get; set; }
+        [Required]
+        [Display(Name = "Description")]
         public virtual string CourseDescription { get; set; }
+        [Required]
+        [Display(Name = "Number of Credits")]
+        [RegularExpression(pattern: "[1234]", ErrorMessage = "Only 1-4 can be entered.")]
         public virtual long CourseCredits { get; set; }
     }
 }
--- EnrollmentApplication/EnrollmentApplication/Models/Student.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -1,9 +1,18 @@
-﻿namespace EnrollmentApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
 {
     public class Student
     {
+        [Display(Name = "Student ID")]
         public virtual long StudentID { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        [MaxLength(length: 50)]
         public virtual string StudentFirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        [MaxLength(length: 50)]
         public virtual string StudentLastName { get; set; }
     }
 }

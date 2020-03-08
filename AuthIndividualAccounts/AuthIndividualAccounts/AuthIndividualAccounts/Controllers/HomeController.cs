84232156ec7dd6bb6e1ae2851381d7aa7c3b38d6
Daniel Norris <Daniel.J.Norris@icloud.com>
2020-03-07 20:36:04 -0500

    Lab 8 Commit and Push

---
+++ AuthIndividualAccounts/AuthIndividualAccounts/AuthIndividualAccounts/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace AuthIndividualAccounts.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
